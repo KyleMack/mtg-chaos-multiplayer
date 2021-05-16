93f4b6614a5cf7ee9bf8027b99a18cce3d7f775d
Kyle MacKenzie <elykcam@gmail.com>
2021-05-16 19:13:47 -0300

    k

--- src/db/createDatabase.sql
+++ src/db/createDatabase.sql
@@ -6,6 +6,3 @@ CREATE DATABASE chaos_db;
 
 CREATE USER chaos_user WITH PASSWORD 'snowwhale420hotdog';
 CREATE USER chaos_admin WITH PASSWORD 'hotdigcatwallbearscoop';
-
-GRANT SELECT ON ALL TABLES IN SCHEMA chaos_db TO chaos_user;
-GRANT ALL PRIVILEGES ON chaos_db TO chaos_admin;
