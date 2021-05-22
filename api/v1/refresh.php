d069c527e5bee66670eae7ea50555b0477775098
Kyle MacKenzie <elykcam@gmail.com>
2021-05-22 00:01:12 -0300

    K

--- api/v1/refresh.php
+++ api/v1/refresh.php
@@ -10,33 +10,19 @@ $T_GAMERULES   = "game_rules";
 
 $conn = New DatabaseConn();
 
-//Delete and re-create the tables
-echo "DELETING TABLES...<BR/>";
 
-echo "DELETING PLAYERS...<BR/>";
-$conn->dropTable($T_PLAYERS);
+echo "CLEARING TABLES...<BR/>";
 
-echo "DELETING GAMES...<BR/>";
-$conn->dropTable($T_ACTIVEGAMES);
-
-echo "DELETING PLAYERS LIST...<BR/>";
-$conn->dropTable($T_GAMEPLAYERS);
-
-echo "DELETING RULES LIST...<BR/>";
-$conn->dropTable($T_GAMERULES;
-
-echo "CREATING TABLES...<BR/>";
-
-echo "CREATING PLAYERS...<BR/>";
+echo "CLEARING PLAYERS...<BR/>";
 $conn->clearTable($T_PLAYERS);
 
-echo "CREATING GAMES...<BR/>";
+echo "CLEARING GAMES...<BR/>";
 $conn->clearTable($T_ACTIVEGAMES);
 
-echo "CREATING PLAYERS LIST...<BR/>";
+echo "CLEARING PLAYERS LIST...<BR/>";
 $conn->clearTable($T_GAMEPLAYERS);
 
-echo "CREATING RULES LIST...<BR/>";
-$conn->clearTable($T_GAMERULES;
+echo "CLEARING RULES LIST...<BR/>";
+$conn->clearTable($T_GAMERULES);
 
 ?>
