c629a8d59bdb3431102527314c359514277cb252
Kyle MacKenzie <elykcam@gmail.com>
2021-05-29 17:29:11 -0300

    Create Load Rule Script

--- api/v1/loadRules.php
+++ api/v1/loadRules.php
@@ -15,7 +15,10 @@ $rPunishment    = loadJSONFile('../data/rulesets/punishmentLand.json');
 $rWacky         = loadJSONFile('../data/rulesets/wackyLand.json');
 
 
-//Add a test rule
-echo $conn->addRule("TEST", "This is a test rule");
-
-
+echo "<p>Adding Rule TEST</p>"; $conn->addRule("TEST", "This is a test rule");
+foreach ($rChaos as $code => $text){ echo "<p>Adding Rule $code</p>"; $conn->addRule($code, $text); }
+foreach ($rDurgs as $code => $text){ echo "<p>Adding Rule $code</p>"; $conn->addRule($code, $text); }
+foreach ($rEnchant as $code => $text){ echo "<p>Adding Rule $code</p>"; $conn->addRule($code, $text); }
+foreach ($rPersona as $code => $text){ echo "<p>Adding Rule $code</p>"; $conn->addRule($code, $text); }
+foreach ($rPunishment as $code => $text){ echo "<p>Adding Rule $code</p>"; $conn->addRule($code, $text); }
+foreach ($rWacky as $code => $text){ echo "<p>Adding Rule $code</p>"; $conn->addRule($code, $text); }
