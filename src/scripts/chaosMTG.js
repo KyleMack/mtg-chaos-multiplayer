fee05f213b826ea43ac614d957296f97ee9eb990
Kyle MacKenzie <elykcam@gmail.com>
2021-05-29 14:23:38 -0300

    Rearrange functions

--- src/scripts/chaosMTG.js
+++ src/scripts/chaosMTG.js
@@ -1,6 +1,53 @@
 "use strict";
 
- 
+
+//Displays the loading screen
+function displayLoadingScreen(){
+    updateContent( getLoadingContent() );
+}
+
+//Displays the 'Join Room' input screen
+function displayJoinRoomScreen(){
+    updateContent( getJoinRoomContent() );
+}
+
+//Updates the loading screen to display a success message
+function displaySuccessMessage(gameCode, playerName){
+    document.querySelector("#loading-message").innerHTML = "Finalizing Game...";
+    document.querySelector("#loading-text").innerHTML = `Finalizing game [${gameCode}] for player [${playerName}]...`;
+}
+
+//Updates the loading screen to display an error
+function displayErrorMessage(){
+    document.querySelector("#loading-message").innerHTML = "Error creating game...";
+    document.querySelector("#loading-text").innerHTML = `Something went wrong, please contact the administrator or try again later`;
+}
+
+//Creates a spinning card element
+function createCard(){
+    //Get the card element
+    let card = getCardTemplate();
+}
+
+//Displays the controls page once the game is loaded
+function displayControls(){
+    updateContent( getControlsContent() );
+}
+
+//Updates all information on the screen with what is stored in the window.chaos object
+function updateScreen(){
+
+    //Display Room Code
+    document.querySelector("#room-code-display").innerHTML = getGameCode();
+
+    //Display Active Players
+    document.querySelector("#room-players-display").innerHTML = createHTMLList( getPlayersList() );
+
+    //Display Active Rules
+    document.querySelector("#room-rules-display").innerHTML = createHTMLList( getActiveRules() );
+
+}
+
 
 //Begins the process of creating a new room from the login page
 async function createNewRoom(){
@@ -55,51 +102,4 @@ async function updateGame(updateScreen = false){
                 updateScreen();
             }
         });
-}
-
-//Displays the loading screen
-function displayLoadingScreen(){
-    updateContent( getLoadingContent() );
-}
-
-//Displays the 'Join Room' input screen
-function displayJoinRoomScreen(){
-    updateContent( getJoinRoomContent() );
-}
-
-//Updates the loading screen to display a success message
-function displaySuccessMessage(gameCode, playerName){
-    document.querySelector("#loading-message").innerHTML = "Finalizing Game...";
-    document.querySelector("#loading-text").innerHTML = `Finalizing game [${gameCode}] for player [${playerName}]...`;
-}
-
-//Updates the loading screen to display an error
-function displayErrorMessage(){
-    document.querySelector("#loading-message").innerHTML = "Error creating game...";
-    document.querySelector("#loading-text").innerHTML = `Something went wrong, please contact the administrator or try again later`;
-}
-
-//Creates a spinning card element
-function createCard(){
-    //Get the card element
-    let card = getCardTemplate();
-}
-
-//Displays the controls page once the game is loaded
-function displayControls(){
-    updateContent( getControlsContent() );
-}
-
-//Updates all information on the screen with what is stored in the window.chaos object
-function updateScreen(){
-
-    //Display Room Code
-    document.querySelector("#room-code-display").innerHTML = getGameCode();
-
-    //Display Active Players
-    document.querySelector("#room-players-display").innerHTML = createHTMLList( getPlayersList() );
-
-    //Display Active Rules
-    document.querySelector("#room-rules-display").innerHTML = createHTMLList( getActiveRules() );
-
 }
